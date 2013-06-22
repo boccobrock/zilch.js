d04fd4130557ebe3a43460ea2e0580e8cb19b5f2
Jeffrey Brock <boccobrock@gmail.com>
2013-06-21 21:03:01 -0600

    demo stuff to get started

---
+++ server_zilch.js
@@ -0,0 +1,22 @@
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    static = require('node-static');
+
+var fileServer = new static.Server('./');
+
+app.listen(8000);
+
+function handler (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response);
+    });
+    request.resume();
+}
+
+// io.set('log level', 1);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('mousemove', function (data) {
+        socket.broadcast.emit('moving', data);
+    });
+});
